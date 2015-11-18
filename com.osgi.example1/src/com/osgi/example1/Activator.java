cbcd66a7f65cdf09493f79ee5d56b2f4afa0c173
Yang Yang <yangyang4cs@gmail.com>
2015-11-17 17:30:09 -0800

    osgi HttpService sample code.

--- com.osgi.example1/src/com/osgi/example1/Activator.java
+++ com.osgi.example1/src/com/osgi/example1/Activator.java
@@ -2,7 +2,14 @@ package com.osgi.example1;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpService;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
+/**
+ * -Dorg.osgi.service.http.port=9090
+ */
 public class Activator implements BundleActivator {
 
 	private static BundleContext context;
@@ -11,16 +18,44 @@ public class Activator implements BundleActivator {
 		return context;
 	}
 
+	protected ServiceTracker<HttpService, HttpService> httpServiceTracker;
+
 	@Override
 	public void start(BundleContext bundleContext) throws Exception {
+		Printer.pl("Activator.start()");
 		Activator.context = bundleContext;
-		System.out.println("Activator.start()");
+
+		this.httpServiceTracker = new ServiceTracker<HttpService, HttpService>(bundleContext, HttpService.class, new ServiceTrackerCustomizer<HttpService, HttpService>() {
+			@Override
+			public HttpService addingService(ServiceReference<HttpService> reference) {
+				Printer.pl("Activator.httpServiceTracker.addingService()");
+				HttpService httpService = getContext().getService(reference);
+				Printer.pl("\t httpService = " + httpService);
+				return httpService;
+			}
+
+			@Override
+			public void modifiedService(ServiceReference<HttpService> reference, HttpService httpService) {
+				Printer.pl("Activator.httpServiceTracker.modifiedService()");
+				Printer.pl("\t httpService = " + httpService);
+			}
+
+			@Override
+			public void removedService(ServiceReference<HttpService> reference, HttpService httpService) {
+				Printer.pl("Activator.httpServiceTracker.removedService()");
+				Printer.pl("\t httpService = " + httpService);
+			}
+		});
+		this.httpServiceTracker.open();
 	}
 
 	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
+		Printer.pl("Activator.stop()");
+
+		this.httpServiceTracker.close();
+
 		Activator.context = null;
-		System.out.println("Activator.stop()");
 	}
 
 }
