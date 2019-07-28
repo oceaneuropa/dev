15ae3eda583bbc7deedc4bb3c6ba55140928e280
Yang Yang <yangyang4cs@gmail.com>
2019-07-28 10:47:13 -0700

    test

---
+++ org.orbit.component.io/src/org/orbit/component/io/util/DefaultNodeControlClientResolver.java
@@ -0,0 +1,16 @@
+package org.orbit.component.io.util;
+
+import java.io.IOException;
+
+import org.orbit.component.api.tier3.nodecontrol.NodeControlClient;
+import org.orbit.component.api.tier3.nodecontrol.NodeControlClientResolver;
+
+public class DefaultNodeControlClientResolver implements NodeControlClientResolver {
+
+	@Override
+	public NodeControlClient resolve(String accessToken, String platformId) throws IOException {
+		NodeControlClient nodeControlClient = OrbitClientHelper.INSTANCE.getNodeControlClient(accessToken, platformId);
+		return nodeControlClient;
+	}
+
+}
---
+++ org.orbit.component.io/src/org/orbit/component/io/util/DefaultPlatformClientResolver.java
@@ -0,0 +1,39 @@
+package org.orbit.component.io.util;
+
+import java.io.IOException;
+
+import org.orbit.infra.api.indexes.IndexItem;
+import org.orbit.infra.api.util.InfraClientsUtil;
+import org.orbit.platform.api.PlatformClient;
+import org.orbit.platform.api.PlatformClientResolver;
+
+public class DefaultPlatformClientResolver implements PlatformClientResolver {
+
+	protected String accessToken;
+
+	/**
+	 * 
+	 * @param accessToken
+	 */
+	public DefaultPlatformClientResolver(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	@Override
+	public PlatformClient resolve(String parentPlatformId, String platformId) throws IOException {
+		return resolve(parentPlatformId, platformId, (String[]) null);
+	}
+
+	@Override
+	public PlatformClient resolve(String parentPlatformId, String platformId, String... platformTypes) throws IOException {
+		PlatformClient platformClient = null;
+
+		IndexItem platformIndexItem = InfraClientsUtil.INDEX_SERVICE.getIndexItem(this.accessToken, parentPlatformId, platformId, platformTypes);
+		if (platformIndexItem != null) {
+			platformClient = OrbitClientHelper.INSTANCE.getPlatformClient(this.accessToken, platformIndexItem);
+		}
+
+		return platformClient;
+	}
+
+}
